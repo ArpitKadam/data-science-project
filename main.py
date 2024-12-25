a2509a54c2735a57ad24ee76ac3e39b09077f7d4
Arpit Kadam <arpitkadam922@gmail.com>
2024-12-25 19:23:56 +0530

    Commiting first project log"

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
 from src.datascienceproject import logger
 
-logger.info("This is an info message in data-science-project")
-logger.error("This is an error message")
-logger.warning("This is a warning message")
-logger.debug("This is a debug message")
+logger.info("Welcome to data-science-project. The project is started and is in progress. P.S:- This is an info message in data-science-project ")
