cda0562b7c47fee5be4e960ed5873bddd47a2954
Arpit Kadam <arpitkadam922@gmail.com>
2024-12-25 19:17:52 +0530

    Random commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from src.datascienceproject import logger
+
+logger.info("This is an info message in data-science-project")
+logger.error("This is an error message")
+logger.warning("This is a warning message")
+logger.debug("This is a debug message")
